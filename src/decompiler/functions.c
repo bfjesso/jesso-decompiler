3b08052ad790a2ab56b74616ab3e5dde2d31fce4
bfjesso <bennettjesso@yahoo.com>
2026-07-08 12:03:40 -0500

    use new instruction index when getting stack offset from other reg

--- src/decompiler/functions.c
+++ src/decompiler/functions.c
@@ -478,14 +478,15 @@ unsigned char isMemAddressStackVar(struct DecompilationParameters* params, int i
 		return 0;
 	}
 
-	enum Register reg = memAddress->reg;
 	if (compareRegisters(memAddress->reg, BP))
 	{
-		reg = BP;
+		if (stackOffset) { *stackOffset = memAddress->constDisplacement; }
+		return 1;
 	}
 	else if (compareRegisters(memAddress->reg, SP))
 	{
-		reg = SP;
+		if (stackOffset) { *stackOffset = memAddress->constDisplacement - getStackFrameSizeAtInstruction(params, instructionIndex); }
+		return 1;
 	}
 	else 
 	{
@@ -496,17 +497,17 @@ unsigned char isMemAddressStackVar(struct DecompilationParameters* params, int i
 			if (instruction->opcode == MOV && instruction->numOfOperands == 2 &&
 				instruction->operands[0].type == REGISTER && instruction->operands[1].type == REGISTER) 
 			{
-				if (compareRegisters(instruction->operands[0].reg, reg)) 
+				if (compareRegisters(instruction->operands[0].reg, memAddress->reg))
 				{
 					if (compareRegisters(instruction->operands[1].reg, BP))
 					{
-						reg = BP;
-						break;
+						if (stackOffset) { *stackOffset = memAddress->constDisplacement; }
+						return 1;
 					}
 					else if (compareRegisters(instruction->operands[1].reg, SP))
 					{
-						reg = SP;
-						break;
+						if (stackOffset) { *stackOffset = memAddress->constDisplacement - getStackFrameSizeAtInstruction(params, i); }
+						return 1;
 					}
 					else 
 					{
@@ -515,17 +516,6 @@ unsigned char isMemAddressStackVar(struct DecompilationParameters* params, int i
 				}
 			}
 		}
-	}
-
-	if (reg == BP)
-	{
-		if (stackOffset) { *stackOffset = memAddress->constDisplacement; }
-		return 1;
-	}
-	else if (reg == SP)
-	{
-		if (stackOffset) { *stackOffset = memAddress->constDisplacement - getStackFrameSizeAtInstruction(params, instructionIndex); }
-		return 1;
 	}
 
 	return 0;
