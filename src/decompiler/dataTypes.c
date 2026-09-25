7b1951608630ae37a77c6ab745889a0c7b813614
bfjesso <bennettjesso@yahoo.com>
2026-09-25 17:37:31 -0500

    check for ptr type when comparing data types

--- src/decompiler/dataTypes.c
+++ src/decompiler/dataTypes.c
@@ -34,8 +34,14 @@ void dataTypeToStr(struct DataType dataType, struct JdcStr* result)
 	}
 }
 
-unsigned char compareDataTypes(struct DataType t1, struct DataType t2)
+unsigned char compareDataTypes(struct DataType t1, struct DataType t2, unsigned char is64Bit)
 {
+	if (((t1.pointerLevel > 0 || t1.arrayLen > 1) && t2.primitiveType == is64Bit ? LONG_LONG_TYPE : INT_TYPE) || 
+		((t2.pointerLevel > 0 || t2.arrayLen > 1) && t1.primitiveType == is64Bit ? LONG_LONG_TYPE : INT_TYPE))
+	{
+		return 1;
+	}
+
 	return t1.primitiveType == t2.primitiveType && t1.isUnsigned == t2.isUnsigned && t1.pointerLevel == t2.pointerLevel && t1.arrayLen == t2.arrayLen;
 }
 
